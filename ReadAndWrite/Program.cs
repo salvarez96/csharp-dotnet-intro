5da798aed3bd4da0f0327071de44b651ac1da23b
Santiago Alvarez <santiagoalvarez9617@gmail.com>
2025-08-22 21:11:00 -0500

    ⚗️ Create program for basic console interaction
    Learn how to get data from the terminal. Learn the best practices to parse numbers

---
+++ ReadAndWrite/Program.cs
@@ -0,0 +1,14 @@
+﻿string? nombre;
+string? apellido;
+byte edad;
+
+Console.Write("Ingrese su nombre: ");
+nombre = Console.ReadLine() ?? "";
+
+Console.Write("Ingrese su apellido: ");
+apellido = Console.ReadLine() ?? "";
+
+Console.Write("Ingrese su edad: ");
+byte.TryParse(Console.ReadLine(), out edad);
+
+Console.WriteLine($"Mi nombre es {nombre} {apellido} y tengo {edad} años");
