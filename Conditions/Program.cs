6d4422e485fb21f309fdff9b8a77f833016db9b8
Santiago Alvarez <santiagoalvarez9617@gmail.com>
2025-08-22 21:12:16 -0500

    ⚗️ Learn how conditions are used in C#

---
+++ Conditions/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Hello, World!");
+
+byte edad = 0;
+
+Console.Write("Introduce tu edad");
+byte.TryParse(Console.ReadLine(), out edad);
+
+if (edad >= 18)
+  Console.WriteLine("Eres mayor de edad");
+else if (edad == 18)
+  Console.WriteLine("Tienes 18 años");
+else
+  Console.WriteLine("Eres menor de edad");
