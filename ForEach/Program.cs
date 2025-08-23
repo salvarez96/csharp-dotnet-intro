80b0c6773afac58db6d0aa31eded3e31804ff2ed
Santiago Alvarez <santiagoalvarez9617@gmail.com>
2025-08-22 21:14:34 -0500

    ⚗️ Learn how to use foreach for iterables in C#

---
+++ ForEach/Program.cs
@@ -0,0 +1,18 @@
+﻿string message = "Hello, World!";
+
+foreach (char c in message)
+{
+  Console.WriteLine(c);
+}
+
+int[] numbers = [ 1, 2, 3, 4, 5 ];
+foreach (int number in numbers)
+{
+  Console.WriteLine(number);
+}
+
+string[] names = [ "John", "Jane", "Bob" ];
+foreach (string name in names)
+{
+  Console.WriteLine(name);
+}
