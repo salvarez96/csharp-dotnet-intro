0822c5358184a0e3a3f2ee9b718cc12c9616dc32
Santiago Alvarez <santiagoalvarez9617@gmail.com>
2025-08-22 21:13:01 -0500

    ⚗️ Learn how cycles work in C#

---
+++ Cycles/Program.cs
@@ -0,0 +1,18 @@
+﻿int counter = 10;
+
+for (int i = 0; i < counter; i++)
+{
+  Console.WriteLine($"Contador for: {i}");
+}
+
+while (counter > 0)
+{
+  Console.WriteLine($"Contador while: {counter}");
+  counter--;
+}
+
+do
+{
+  counter++;
+  Console.WriteLine($"Contador do-while: {counter}");
+} while (counter < 10);
