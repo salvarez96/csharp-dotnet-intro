db9bee1edd9f46a8325061288e22d7558f6bf64d
Santiago Alvarez <santiagoalvarez9617@gmail.com>
2025-08-22 21:08:35 -0500

    ⚗️ Create first HelloWorld C# program
    Create first console program. Learn about C# primitives and how to send messages to the console

---
+++ HolaMundo/Program.cs
@@ -0,0 +1,10 @@
+﻿const string NOMBRE = "Santiago";
+string apellido = "Alvarez";
+
+int edad;
+edad = 29;
+
+double altura = 1.78;
+
+Console.WriteLine("Hola " + NOMBRE + " " + apellido);
+Console.WriteLine("Tienes " + edad + " años y mides " + altura + " metros");
