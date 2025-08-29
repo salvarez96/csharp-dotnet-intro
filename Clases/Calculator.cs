18875ec614ee80a9a821db583fc633ab2a653f9e
Santiago Alvarez <santiagoalvarez9617@gmail.com>
2025-08-29 14:56:42 -0500

    ⚗️ Create and implement a static class
    Fix some bugs in Calculator class

--- Clases/Calculator.cs
+++ Clases/Calculator.cs
@@ -1,33 +1,45 @@
+// Compiler let's us use a class outside a namespace, but it is a bad practice in general
+public static class StaticCalculator
+{
+  public static int Sum(int num1, int num2) => num1 + num2;
+
+  public static int Rest(int num1, int num2) => num1 - num2;
+
+  public static int Multiply(int num1, int num2) => num1 * num2;
+
+  public static double Divide(double num1, double num2) => num1 / num2;
+}
+
 namespace Operations
 {
   public class Calculator
   {
-    public void Sum(int num1, int num2)
+    public string Sum(int num1, int num2)
     {
       int res = num1 + num2;
-      OperationResult("suma", res);
+      return OperationResult("suma", res);
     }
 
-    public void Rest(int num1, int num2)
+    public string Rest(int num1, int num2)
     {
       int res = num1 - num2;
-      OperationResult("resta", res);
+      return OperationResult("resta", res);
     }
 
-    public void Multiply(int num1, int num2)
+    public string Multiply(int num1, int num2)
     {
       int res = num1 * num2;
-      OperationResult("multiplicación", res);
+      return OperationResult("multiplicación", res);
     }
 
-    public void Divide(int num1, int num2)
+    public string Divide(double num1, double num2)
     {
-      int res = num1 / num2;
-      int remainder = num1 % num2;
-      OperationResult("", res, $"La division de los números {num1} y {num2} es {res} y sobra {remainder}");
+      double res = num1 / num2;
+      double remainder = num1 % num2;
+      return OperationResult("", res, $"La division de los números {num1} y {num2} es {res} y sobra {remainder}");
     }
 
-    private string OperationResult(string operationType, int result, string? customMessage = null)
+    private string OperationResult(string operationType, double result, string? customMessage = null)
     {
       return customMessage ?? $"El resultado de la operación {operationType} es {result}";
     }
--- Clases/Program.cs
+++ Clases/Program.cs
@@ -1,7 +1,12 @@
 ﻿using Operations;
 
 Calculator calculator = new();
-calculator.Sum(1, 2);
-calculator.Rest(1, 2);
-calculator.Multiply(1, 2);
-calculator.Divide(1, 2);
+Console.WriteLine(calculator.Sum(1, 2));
+Console.WriteLine(calculator.Rest(1, 2));
+Console.WriteLine(calculator.Multiply(1, 2));
+Console.WriteLine(calculator.Divide(1, 2));
+
+Console.WriteLine($"Using static calculator to sum: {StaticCalculator.Sum(1, 2)}");
+Console.WriteLine($"Using static calculator to rest: {StaticCalculator.Rest(1, 2)}");
+Console.WriteLine($"Using static calculator to multiply: {StaticCalculator.Multiply(1, 2)}");
+Console.WriteLine($"Using static calculator to divide: {StaticCalculator.Divide(1, 2)}");
