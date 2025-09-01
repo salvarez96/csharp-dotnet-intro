9ab8e0ee1a62069bdf5ca1d24227970c78259feb
Santiago Alvarez <santiagoalvarez9617@gmail.com>
2025-09-01 15:11:27 -0500

    🔧 Create debug configuration files for specific projects

---
+++ Debugging/Program.cs
@@ -0,0 +1,19 @@
+﻿List<string> names = new();
+
+names.Add("Santiago");
+names.Add("Felipe");
+names.Add("Carlos");
+
+List<string> surnames = new();
+
+surnames.Add("Alvarez");
+surnames.Add("Gomez");
+surnames.Add("Perez");
+
+names.ForEach(name =>
+{
+  surnames.ForEach(surname =>
+  {
+    Console.WriteLine($"{name} {surname}");
+  });
+});
