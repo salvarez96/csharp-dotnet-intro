515b0707032745b37de46fe050f7ae7b5b9ccf3a
Santiago Alvarez <santiagoalvarez9617@gmail.com>
2025-09-01 17:07:28 -0500

    ➕ Learn how to use dependencies
    Install Newtonsoft.json and use it to serialize an object

---
+++ Nuget/Program.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+
+public class Person
+{
+  public required string Name { get; set; }
+  public required int Age { get; set; }
+}
+
+class Program
+{
+  static void Main(string[] args)
+  {
+    Person person = new Person
+    {
+      Name = "Santiago",
+      Age = 29
+    };
+
+    string json = JsonConvert.SerializeObject(person);
+    Console.WriteLine($"Serialized JSON: {json}");
+
+    Person deserializedPerson = JsonConvert.DeserializeObject<Person>(json)!;
+    Console.WriteLine($"Deserialized Person: Name = {deserializedPerson.Name}, Age = {deserializedPerson.Age}");
+  }
+}
